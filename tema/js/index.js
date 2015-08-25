d6def92d365cb2f5141cafb1b7301c42c9f8f3dc
krebosio18 <kevin@picnic.pe>
2015-08-24 20:23:54 -0500

    asdasdasd

---
+++ tema/js/index.js
@@ -0,0 +1,8 @@
+$(function(){
+
+	setTimeout(cargarPagina, 5000);
+
+	function cargarPagina(){
+		window.location.href = "intro.html";
+	}
+});
---
+++ tema/js/temafondo.js
@@ -0,0 +1,18 @@
+$(function(){
+
+	var btnCategoria	=	$('.btn__categoria');
+
+	$.getJSON("./data/categorias.json", function(objcategorias){
+		$.each(objcategorias.categorias, function(posicion,categoria){
+			if(categoria.id==3){
+				$('#nameCategory').append(categoria.nombre);
+				$.each(categoria.data, function(indice,cat){
+					
+					$('#popularidad').append('<li><a href="'+cat.file+'">'+cat.nombre+'</a></li>');
+				});
+			}
+			
+		});
+	});
+
+});
